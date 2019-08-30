4e75bb13237a13706e5da2fe233dab8d3152f28d
Yahya <yahyatinani@gmail.com>
2019-08-30 19:21:47 +0100

    Refactor the type label

--- lib/widgets/type_label.dart
+++ lib/widgets/type_label.dart
@@ -1,36 +1,38 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TypeLabel extends StatelessWidget {
   final String type;
   final Color color;
   final double width;
-  final double height;
   final double fontSize;
 
   const TypeLabel(
     this.type, {
     this.color,
     this.fontSize = 20,
-    this.height = 32,
     this.width = 180,
   });
 
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: ScreenUtil.getInstance().setHeight(5),
+        ),
         alignment: Alignment.center,
-        width: width,
-        height: height,
         decoration: BoxDecoration(
           color: color,
           borderRadius: BorderRadius.circular(100),
         ),
-        child: Text(
+        child: AutoSizeText(
           type,
+          maxLines: 1,
           style: TextStyle(
             color: Colors.white,
-            fontSize: fontSize,
-            fontWeight: FontWeight.bold,
+            fontSize: ScreenUtil.getInstance().setSp(32),
+            fontWeight: FontWeight.w600,
           ),
         ),
       ),
