0aa144a4e1fefa9c938a29d7645df2264100cca7
Yahya <yahyatinani@gmail.com>
2019-09-20 18:58:34 +0100

    Fix hidden ability size bug

--- lib/widgets/hidden_ability_text.dart
+++ lib/widgets/hidden_ability_text.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HiddenAbilityText extends StatelessWidget {
   final Color color;
@@ -9,7 +10,11 @@ class HiddenAbilityText extends StatelessWidget {
   Widget build(BuildContext context) {
     return AutoSizeText(
       ' (hidden ability)',
-      style: TextStyle(color: color, fontStyle: FontStyle.italic),
+      style: TextStyle(
+        color: color,
+        fontStyle: FontStyle.italic,
+        fontSize: ScreenUtil.getInstance().setSp(24),
+      ),
     );
   }
 }
